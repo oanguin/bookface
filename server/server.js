9fb427292b827191f078f162fc876a5c225c2541
Oneal Anguin <onealanguin@Oneals-MacBook-Pro.local>
2018-10-19 22:43:12 +0100

    Work on author end point.

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ var _ = require("lodash");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
@@ -19,18 +23,41 @@ let authors = [];
 let id = 0;
 
 app.get(`/${AUTHOR}`, (req, res) => {
-  res.setHeader(200);
+  res.statusCode = 200;
   res.json(authors);
 });
 
 app.post(`/${AUTHOR}`, (req, res) => {
   let author = req.body;
   id++;
-  author.id = id;
+  author.id = id + "";
 
-  authors.pop(author);
-
+  authors.push(author);
+  res.statusCode = 200;
   res.send(author);
+});
+
+app.patch(`/${AUTHOR}/:id`, (req, res) => {
+  let existingAuthorIndex = _.findIndex(authors, { id: req.params.id });
+
+  let editedAuthor = req.body;
+  if (editedAuthor.id) {
+    delete editedAuthor.id;
+  }
+
+  if (!authors[existingAuthorIndex]) {
+    res.statusCode = 404;
+    res.json("Not Found");
+  } else {
+    res.statusCode = 201;
+    /*Using new spread operator to merge objects.
+    The right most object values with overwrite existing values. */
+    authors[existingAuthorIndex] = {
+      ...authors[existingAuthorIndex],
+      ...editedAuthor
+    };
+    res.json(authors[existingAuthorIndex]);
+  }
 });
 /*End of Author Rleated Endpoints*/
 
