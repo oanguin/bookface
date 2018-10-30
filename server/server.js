793a2f6196c52235d0b1674f6c61f7d6f65372dd
Oneal Anguin <oneal.anguin@ef.com>
2018-10-30 11:40:16 +0000

    Included router and seperated controllers.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var morgan = require("morgan");
 var fs = require("fs");
 var path = require("path");
 var rfs = require("rotating-file-stream");
+var auhtors = require("./author/authors");
 
 const app = express();
 const port = 3000;
@@ -50,77 +51,21 @@ app.use(bodyParser.json());
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  res.send("Hi from book app.");
-});
-
-/*Author Rleated Endpoints - Will need to be moved later*/
+/*START Rest End Point Routes*/
 const AUTHOR = "authors";
+app.use(`/${AUTHOR}`, auhtors);
+/*END of End Point Routes*/
 
-let authors = [];
-let id = 0;
+/*START Views*/
+/*app.get("/", (req, res) => {
+  res.render("index");
+});*/
 
-/*The Id will be added to the request body as req.body.id */
-var updateId = function(req, res, next) {
-  id += 1;
-  req.body.id = id + "";
-  next();
-};
-
-/*For every path with id this middleware will be called then next operation
-will take place. A author will be returned in the the request object as req.author */
-app.param("id", (req, res, next, id) => {
-  let index = _.findIndex(authors, {
-    id: req.params.id
-  });
-  req.author = authors[index];
-  next();
-});
-
-app.get(`/${AUTHOR}`, (req, res) => {
-  res.statusCode = 200;
-  res.json(authors);
-});
-
-/*Use updateId function to add id to req.body */
-app.post(`/${AUTHOR}`, updateId, (req, res) => {
-  let author = req.body;
-
-  authors.push(author);
-  res.statusCode = 200;
-  res.send(author);
-});
-
-app.patch(`/${AUTHOR}/:id`, (req, res) => {
-  let existingAuthorIndex = _.findIndex(authors, {
-    id: req.author.id
-  });
-  let editedAuthor = req.body;
-  delete editedAuthor.id;
-
-  if (!editedAuthor) {
-    res.statusCode = 404;
-    res.json("Not Found");
-  } else {
-    res.statusCode = 201;
-    /*Using new spread operator to merge objects.
-    The right most object values with overwrite existing values. */
-    authors[existingAuthorIndex] = {
-      ...authors[existingAuthorIndex],
-      ...editedAuthor
-    };
-
-    res.json(authors[existingAuthorIndex]);
-  }
-});
-
-/*End of Author Rleated Endpoints*/
 app.get("/show_authors", (req, res) => {
   res.render("author/list", { data: authors });
 });
-/*Views*/
-
 /*End of Views */
+
 /*Middleware to catch all errors which are not managed.
 Note that the signiture with the error at the beginning is needed. */
 app.use((err, req, res, next) => {
